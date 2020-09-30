1b7da04b717a8043803f350e4227415db99fbbb5
Ishani <ishaniwadhwana207@gmail.com>
2020-09-30 14:06:47 +0530

    add client

---
+++ client_react/src/components/Profile.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import jwt_decode from 'jwt-decode'
+
+class Profile extends Component {
+  constructor() {
+    super()
+    this.state = {
+      first_name: '',
+      last_name: '',
+      email: '',
+      errors: {}
+    }
+  }
+
+  componentDidMount() {
+    const token = localStorage.usertoken
+    const decoded = jwt_decode(token)
+    this.setState({
+      first_name: decoded.first_name,
+      last_name: decoded.last_name,
+      email: decoded.email
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron mt-5">
+          <div className="col-sm-8 mx-auto">
+            <h1 className="text-center">Welcome to my favour app!</h1>
+          </div>
+          
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Profile
