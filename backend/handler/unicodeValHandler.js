dc3291c0adbee032954a2c3fe844aae2b5a1bc6d
Jinal1995 <72867531+Jinal1995@users.noreply.github.com>
2020-11-02 22:39:54 +1100

    comments added
    comments added on unicode validation handler

--- backend/handler/unicodeValHandler.js
+++ backend/handler/unicodeValHandler.js
@@ -1,4 +1,5 @@
 const { param } = require("../API/partydet");
+//it will handler the regular expression for validation of unicode value.
 module.exports = function uniCodeVal(string) {
     return /[^\u0000-\u007f]/.test(string);
 }
